cd8afab814fa6f6334b8c7884ca37c36e4177cd3
Erik <eekihenriksson@gmail.com>
2023-08-03 21:18:26 +0300

    update config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const cnf = {
-  api: "https://new.papygreek.com/api",
-  arethusaedit: "https://new.papygreek.com/arethusa/app/#/papygreek",
-  arethusaview: "https://new.papygreek.com/arethusa/app/#/papygreek_review",
+  api: "https://papygreek.com/api",
+  arethusaedit: "https://papygreek.com/arethusa/app/#/papygreek",
+  arethusaview: "https://papygreek.com/arethusa/app/#/papygreek_review",
   google:
     "139191467633-bojga6dr51j361aup3beikqkqcpnquf6.apps.googleusercontent.com",
 };
